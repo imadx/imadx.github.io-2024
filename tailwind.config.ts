64b7c472bd173a8bc7fef53a171d7d979dca5e64
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2024-05-19 19:36:30 +0530

    update colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        shark: {
+          "50": "#f4f6f7",
+          "100": "#e3e7ea",
+          "200": "#cbd3d6",
+          "300": "#a6b4ba",
+          "400": "#7a8c96",
+          "500": "#5f717b",
+          "600": "#515f69",
+          "700": "#465058",
+          "800": "#3e454c",
+          "900": "#373d42",
+          "950": "#212529",
+        },
+      },
     },
   },
   plugins: [],
