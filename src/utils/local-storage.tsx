18c5b4ad1e5bceb9e2d5a8530b1ab80f6989990d
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2024-05-19 19:02:04 +0530

    fix issue with the client-side utils

--- src/utils/local-storage.tsx
+++ src/utils/local-storage.tsx
@@ -5,10 +5,14 @@ export enum LocalStorageKey {
 const LocalStoragePrefix = "ishan-portfolio-";
 
 export function setLocalStorage(key: LocalStorageKey, value: any) {
+  if (typeof window === "undefined") return;
+
   localStorage.setItem(LocalStoragePrefix + key, JSON.stringify(value));
 }
 
 export function getLocalStorage<T>(key: LocalStorageKey): T | null {
+  if (typeof window === "undefined") return null;
+
   const value = localStorage.getItem(LocalStoragePrefix + key);
   return value ? JSON.parse(value) : null;
 }
