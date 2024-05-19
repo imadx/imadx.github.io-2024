2ea84c3b16c553ccc431ba90471ac8cdc361d48b
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2024-05-19 20:01:11 +0530

    update whoami section

--- src/app/_components/shell-section-who-am-i.tsx
+++ src/app/_components/shell-section-who-am-i.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { ShellSection } from "./shell-section";
+import { AtSymbolIcon, PhoneIcon, LinkIcon } from "@heroicons/react/24/outline";
 
 const contactDetails = {
   name: "Ishan Madhusanka",
@@ -8,23 +9,50 @@ const contactDetails = {
   githubHandle: "imadx",
 };
 
+interface ExternalLinkProps {
+  icon: JSX.Element;
+  href: string;
+  label: string;
+}
+
+const ExternalLink: FC<ExternalLinkProps> = ({ icon, href, label }) => {
+  return (
+    <a
+      href={href}
+      className="hover:text-lime-300 py-2 flex gap-2 items-center transition-colors"
+      target="_blank"
+      rel="nofollow"
+    >
+      {icon} {label}
+    </a>
+  );
+};
+
 export const ShellSectionWhoAmI: FC = () => {
   return (
     <ShellSection command="whoami">
-      <h1 className="text-4xl my-3">{contactDetails.name}</h1>
-      <address>
-        Email:{" "}
-        <a href={`mailto:${contactDetails.email}`}>{contactDetails.email}</a>
-      </address>
-      <address>
-        Phone:{" "}
-        <a href={`tel:${contactDetails.phone.replaceAll(" ", "")}`}>
-          ${contactDetails.phone}
-        </a>
-      </address>
-      <a href={`https://github.com/${contactDetails.githubHandle}`}>
-        GitHub Profile: github.com/{contactDetails.githubHandle}
-      </a>
+      <div className="flex flex-col">
+        <h1 className="text-4xl my-3">{contactDetails.name}</h1>
+        <address>
+          <ExternalLink
+            icon={<AtSymbolIcon className="h-5" />}
+            href={`mailto:${contactDetails.email}`}
+            label={contactDetails.email}
+          />
+        </address>
+        <address>
+          <ExternalLink
+            icon={<PhoneIcon className="h-5" />}
+            href={`tel:${contactDetails.phone.replaceAll(" ", "")}`}
+            label={contactDetails.phone}
+          />
+        </address>
+        <ExternalLink
+          icon={<LinkIcon className="h-5" />}
+          href={`https://github.com/${contactDetails.githubHandle}`}
+          label={`github.com/${contactDetails.githubHandle}`}
+        />
+      </div>
     </ShellSection>
   );
 };
