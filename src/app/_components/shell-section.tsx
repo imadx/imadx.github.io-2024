fc7842ea7e8ac48e2e613ff552584793627db84a
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2024-05-22 21:28:38 +0530

    add support for showing a loading indicator

--- src/app/_components/shell-section.tsx
+++ src/app/_components/shell-section.tsx
@@ -12,6 +12,7 @@ interface ShellSectionProps {
   command?: string;
   children: ReactNode;
   state?: { isFetching: boolean; isError: boolean; isUninitialized: boolean };
+  loadingIndicator?: ReactNode;
   contentType?: ContentType;
 }
 
@@ -20,6 +21,7 @@ export const ShellSection: FC<ShellSectionProps> = ({
   command,
   children,
   state,
+  loadingIndicator,
   contentType,
 }) => {
   const titleSuffix = useMemo(() => {
@@ -36,53 +38,76 @@ export const ShellSection: FC<ShellSectionProps> = ({
     }
   }, [title, contentType]);
 
+  const divClassNames = clsx(
+    "mb-12 group p-4 py-6 -m-4 transition-background rounded-md",
+    "hover:ring-1 hover:ring-black/30",
+  );
+
+  const titleContent = useMemo(() => {
+    return (
+      <>
+        {command && (
+          <h2
+            className={clsx(
+              "opacity-50 truncate mb-3",
+              "before:content-[attr(data-before)] before:opacity-30",
+            )}
+            data-before="~$ "
+            title={command}
+          >
+            {command}
+          </h2>
+        )}
+
+        {title && (
+          <h2
+            className={clsx(
+              "opacity-50 truncate mb-3 transition-opacity",
+              "before:content-[attr(data-before)] before:opacity-30",
+              "after:content-[attr(data-after)] after:opacity-30",
+              "group-hover:text-white group-hover:opacity-100",
+            )}
+            data-before="~$ (cd '"
+            data-after={titleSuffix}
+            title={title}
+          >
+            {title}
+          </h2>
+        )}
+      </>
+    );
+  }, [command, title, titleSuffix]);
+
   if (state?.isFetching) {
-    return <div className="mb-12">Loading...</div>;
+    return (
+      <div className={divClassNames}>
+        {titleContent}
+        {loadingIndicator || "Loading..."}
+      </div>
+    );
   }
 
   if (state?.isError) {
-    return <div className="mb-12">Failed to load data</div>;
+    return (
+      <div className={divClassNames}>
+        {titleContent}
+        Failed to load data
+      </div>
+    );
   }
 
   if (state?.isUninitialized) {
-    return <div className="mb-12">Fetching...</div>;
+    return (
+      <div className={divClassNames}>
+        {titleContent}
+        Fetching...
+      </div>
+    );
   }
 
   return (
-    <div
-      className={clsx(
-        "mb-12 group p-4 py-6 -m-4 transition-background rounded-md",
-        "hover:ring-1 hover:ring-black/30",
-      )}
-    >
-      {command && (
-        <h2
-          className={clsx(
-            "opacity-50 truncate mb-3",
-            "before:content-[attr(data-before)] before:opacity-30",
-          )}
-          data-before="~$ "
-          title={command}
-        >
-          {command}
-        </h2>
-      )}
-
-      {title && (
-        <h2
-          className={clsx(
-            "opacity-50 truncate mb-3 transition-opacity",
-            "before:content-[attr(data-before)] before:opacity-30",
-            "after:content-[attr(data-after)] after:opacity-30",
-            "group-hover:text-white group-hover:opacity-100",
-          )}
-          data-before="~$ (cd '"
-          data-after={titleSuffix}
-          title={title}
-        >
-          {title}
-        </h2>
-      )}
+    <div className={divClassNames}>
+      {titleContent}
       {children}
     </div>
   );
