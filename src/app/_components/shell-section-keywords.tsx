9b8c4ba9b75c1b22d8c89f08a25078bbd4745319
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2024-05-23 21:20:01 +0530

    add keywords to constants

--- src/app/_components/shell-section-keywords.tsx
+++ src/app/_components/shell-section-keywords.tsx
@@ -1,20 +1,7 @@
 import clsx from "clsx";
 import { FC } from "react";
 import { ContentType, ShellSection } from "./shell-section";
-
-const keywords = [
-  "ishan madhusanka",
-  "ishan",
-  "imadhusanka",
-  "ahtimadhusanka",
-  "i.mad",
-  "imadx",
-  "tech lead",
-  "fintech",
-  "software engineer",
-  "full-stack developer",
-  "ui/ux enthusiast",
-];
+import { keywords } from "@/constants/keywords";
 
 export const ShellSectionKeywords: FC = () => {
   return (
---
+++ src/constants/keywords.ts
@@ -0,0 +1,13 @@
+export const keywords = [
+  "ishan madhusanka",
+  "ishan",
+  "imadhusanka",
+  "ahtimadhusanka",
+  "i.mad",
+  "imadx",
+  "tech lead",
+  "fintech",
+  "software engineer",
+  "full-stack developer",
+  "ui/ux enthusiast",
+];
