3636631acd178bbad7ac352c03ab6dfa0b6f4826
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2024-05-24 21:58:48 +0530

    update toggle to show hi/low res label

--- src/app/_components/toggle-mode-content.tsx
+++ src/app/_components/toggle-mode-content.tsx
@@ -2,10 +2,9 @@ import { FC, useState } from "react";
 
 interface ToggleMode {
   onChange: (value: boolean) => void;
-  label: string;
 }
 
-export const ToggleMode: FC<ToggleMode> = ({ onChange, label }) => {
+export const ToggleModeContent: FC<ToggleMode> = ({ onChange }) => {
   const [isOn, setIsOn] = useState(false);
 
   const handleOnClick = () => {
@@ -17,8 +16,15 @@ export const ToggleMode: FC<ToggleMode> = ({ onChange, label }) => {
   };
 
   return (
-    <div className="float-end bg-red-500">
-      <button onClick={handleOnClick}>{label}</button>
+    <div className="float-end font-mono">
+      View in{" "}
+      <button
+        className="bg-shark-900 px-4 py-1 rounded-full"
+        onClick={handleOnClick}
+      >
+        {isOn ? "Lo" : "Hi"}
+      </button>{" "}
+      res
     </div>
   );
 };
